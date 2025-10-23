6536b2d35e10afb37f5596b6b52eaa529ab11a64
KuyPoly <kuypoly297@gmail.com>
2025-10-23 21:23:43 +0700

    week3 lab

--- Week3_lab/lib/main.dart
+++ Week3_lab/lib/main.dart
@@ -1,23 +1,30 @@
- 
 import 'domain/quiz.dart';
 import 'ui/quiz_console.dart';
+import 'data/quizRepository.dart';
 
 void main() {
 
-  List<Question> questions = [
-    Question(
-        title: "Capital of France?",
-        choices: ["Paris", "London", "Rome"],
-        goodChoice: "Paris",
-        point: 10),
-    Question(
-        title: "2 + 2 = ?", 
-        choices: ["2", "4", "5"], 
-        goodChoice: "4"),
-  ];
+  // List<Question> questions = [
+  //   Question(
+  //       title: "Capital of France?",
+  //       choices: ["Paris", "London", "Rome"],
+  //       goodChoice: "Paris",why it cannot fi
+  //       ),
+        
+  //   Question(
+  //       title: "2 + 2 = ?", 
+  //       choices: ["2", "4", "5"], 
+  //       goodChoice: "4",
+  //       point: 50),
+  // ];
 
-  Quiz quiz = Quiz(questions: questions);
+  // Read quiz from JSON file
+  QuizRepository repository = QuizRepository('D:\\y3t1\\mobile dev\\assignment\\Week3_lab\\lib\\data\\quiz.json');
+  Quiz quiz = repository.readQuiz();
+
   QuizConsole console = QuizConsole(quiz: quiz);
-
   console.startQuiz();
+
+  QuizRepository resultsRepo = QuizRepository('D:\\y3t1\\mobile dev\\assignment\\Week3_lab\\lib\\data\\quiz_result.json');
+  resultsRepo.writeQuiz(quiz);
 }
--- Week3_lab/lib/ui/quiz_console.dart
+++ Week3_lab/lib/ui/quiz_console.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import '../domain/quiz.dart';
 
@@ -10,28 +11,52 @@ class QuizConsole {
   void startQuiz() {
     print('--- Welcome to the Quiz ---\n');
 
-    for (var question in quiz.questions) {
-      print('Question: ${question.title} - (${question.point} points)');
-      print('Choices: ${question.choices}');
-      stdout.write('Your answer: ');
-      String? userInput = stdin.readLineSync();
+    // Loop to allow multiple players
+    while (true) {
+      stdout.write("Your name: ");
+      String? playerName = stdin.readLineSync();
 
-      // Check for null input
-      if (userInput != null && userInput.isNotEmpty) {
-        Answer answer = Answer(question: question, answerChoice: userInput);
-        quiz.addAnswer(answer);
-      } else {
-        print('No answer entered. Skipping question.');
+      // Exit if no name is provided
+      if (playerName == null || playerName.trim().isEmpty) {
+        break;
       }
 
-      print('');
+      // Add players to the quiz
+      quiz.addPlayer(Player(name: playerName));
+
+      // Player takes quiz
+      List<Answer> playerAnswers = [];
+
+      for (var question in quiz.questions) {
+        print('Question: ${question.title} - (${question.point} points))');
+        print('Choices: ${question.choices}');
+        stdout.write('Your answer: ');
+        String? userInput = stdin.readLineSync();
+
+        // Check for null input
+        if (userInput != null && userInput.isNotEmpty) {
+          Answer answer = Answer(question: question, answerChoice: userInput);
+          playerAnswers.add(answer);
+        } else {
+          print('No answer entered. Skipping question.');
+        }
+
+        print('');
+      }
+
+      // Calculate score and points for players
+      quiz.players.last.score = quiz.getScoreInPercentage(playerAnswers);
+      quiz.players.last.point = quiz.getPoint(playerAnswers);
+
+      print("${quiz.players.last.name}, your score in percentage: ${quiz.players.last.score} %");
+      print("${quiz.players.last.name}, your score in points: ${quiz.players.last.point}");
+
+      // Display all players scores
+      for (var player in quiz.players) {
+        print("Player: ${player.name}\t\tScore:${player.score}");
+      }
     }
 
-    int score = quiz.getScoreInPercentage();
-    int point = quiz.getPoint();
     print('--- Quiz Finished ---');
-    print('Your score: $score % correct');
-    print('Your score: $point  points');
   }
 }
- 
